b87dfe4dac97814df727e131316137820c9ce317
Martin <pmartin@infinitesquare.com>
2020-12-24 19:23:23 +0100

    renew users list page

--- app/src/pages/UsersList.jsx
+++ app/src/pages/UsersList.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { Card, List, Avatar, Space } from "antd";
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 import { useQuery, gql } from '@apollo/client';
 import AppPage from "./AppPage";
+
+const { Meta } = Card;
 
 const USERS = gql`
     query GetUsers {
@@ -13,6 +17,13 @@ const USERS = gql`
         }
     }
 `;
+
+const IconText = ({ icon, text }) => (
+    <Space>
+        {React.createElement(icon)}
+        {text}
+    </Space>
+);
 
 function UsersList() {
     const { loading, error, data, refetch } = useQuery(USERS);
@@ -30,6 +41,51 @@ function UsersList() {
             <div className="users-list-container paddings">
                 <h2>List of users</h2>
 
+                {
+                    (data && data.users && data.users.length > 0 ) && (
+                        <List
+                            grid={{
+                                gutter: 16
+                            }}
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 10
+                            }}
+                            dataSource={data && data.users}
+                            renderItem={(user) => (
+                                <List.Item>
+                                    <Link to={`/user/${user.id}`}>
+                                        <Card
+                                            style={{ width: 300 }}
+                                            hoverable
+                                            cover={
+                                                <img
+                                                    alt="example"
+                                                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                                                />
+                                            }
+                                            actions={[
+                                                <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                                                <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                                                <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                                            ]}
+                                        >
+                                            <Meta
+                                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                                title={`${user.firstname} ${user.lastname}`}
+                                                description={user.email}
+                                            />
+                                        </Card>
+                                    </Link>
+                                </List.Item>
+                            )}
+                        />
+                    )
+                }
+
+                {/* Old approach:
                 {
                     (data && data.users && data.users.length > 0 ) && (
                         <table>
@@ -58,6 +114,7 @@ function UsersList() {
                         </table>
                     )
                 }
+                */}
             </div>
         </AppPage>
     );
