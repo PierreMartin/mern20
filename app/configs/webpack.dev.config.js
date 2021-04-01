0caccbff755ecfe17bb7d7d4f5d1588faf4c8863
Martin <pmartin@infinitesquare.com>
2021-04-01 22:56:03 +0200

    refacto

--- app/configs/webpack.dev.config.js
+++ app/configs/webpack.dev.config.js
@@ -1,89 +1,91 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const isServerSide = process.env.NODE_RENDERING === 'server';
-const isClientSide = process.env.NODE_RENDERING !== 'server';
 
-module.exports = {
-    mode: 'development',
-    entry: './src/client.jsx',
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.jsx', '.tsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['babel-loader']
-            },
-            /*
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            */
-            {
-                test: [/\.less$/, /\.css$/],
-                use: [
-                    // { loader : MiniCssExtractPlugin.loader},
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'postcss-preset-env', // autoprefix css + browserslist
-                                        {
-                                            browsers: 'last 2 versions'
-                                        }
-                                    ],
-                                    require('tailwindcss'),
-                                    require('autoprefixer')
-                                ]
+module.exports = (env = {}) => {
+    const isClientSide = env.client;
+    const isServerSide = env.server;
+
+    const configuration = {
+        mode: 'development',
+        entry: './src/client.jsx',
+        devtool: 'inline-source-map',
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.jsx', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: ['babel-loader']
+                },
+                /*
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                */
+                {
+                    test: [/\.less$/, /\.css$/],
+                    use: [
+                        // { loader : MiniCssExtractPlugin.loader},
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                                postcssOptions: {
+                                    plugins: [
+                                        [
+                                            'postcss-preset-env', // autoprefix css + browserslist
+                                            {
+                                                browsers: 'last 2 versions'
+                                            }
+                                        ],
+                                        require('tailwindcss'),
+                                        require('autoprefixer')
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            loader: 'less-loader',
+                            options: {
+                                sourceMap: true
                             }
                         }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + './../public/index.html',
+                filename: './index.html',
+                // inject: 'body'
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + './../public/index.html',
-            filename: './index.html',
-            // inject: 'body'
-        })
-    ]
-};
+    };
 
-if (isClientSide) {
-    module.exports.devServer = {
-        inline: true,
+    if (isClientSide) {
+        configuration.devServer = {
+            inline: true,
             port: 3000,
             hot: true,
             historyApiFallback: true
@@ -96,5 +98,8 @@ if (isClientSide) {
                 }
             }
             */
-    };
-}
+        };
+    }
+
+    return configuration;
+};
--- app/configs/webpack.prod.config.js
+++ app/configs/webpack.prod.config.js
@@ -1,79 +1,83 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/client.jsx',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.jsx', '.tsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['babel-loader']
-            },
-            /*
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            */
-            {
-                test: [/\.less$/, /\.css$/],
-                use: [
-                    // { loader : MiniCssExtractPlugin.loader},
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'postcss-preset-env', // autoprefix css + browserslist
-                                        {
-                                            browsers: 'last 2 versions'
-                                        }
-                                    ],
-                                    require('tailwindcss'),
-                                    require('autoprefixer')
-                                ]
+module.exports = (env = {}) => {
+    const isServerSide = env.server;
+    console.log('isServerSideTT >>> ', isServerSide);
+
+    const configuration = {
+        mode: 'production',
+        entry: './src/client.jsx',
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.jsx', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: ['babel-loader']
+                },
+                /*
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                */
+                {
+                    test: [/\.less$/, /\.css$/],
+                    use: [
+                        // { loader : MiniCssExtractPlugin.loader},
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
                             }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                                postcssOptions: {
+                                    plugins: [
+                                        [
+                                            'postcss-preset-env', // autoprefix css + browserslist
+                                            {
+                                                browsers: 'last 2 versions'
+                                            }
+                                        ],
+                                        require('tailwindcss'),
+                                        require('autoprefixer')
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            loader: 'less-loader'
                         }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + './../public/index.html',
+                filename: './index.html',
+                // inject: 'body'
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/public/index.html',
-            filename: './index.html',
-            // inject: 'body'
-        })
-    ]
+    };
+
+    return configuration;
 };
